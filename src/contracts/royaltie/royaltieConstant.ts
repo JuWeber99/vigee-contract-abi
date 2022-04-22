ce6911c62e68cb9d2a352224f0744b95d811e9fd
julian.weber <julian.weber@secuinfra.com>
2022-04-22 21:56:54 +0200

    testing

--- src/contracts/royaltie/royaltieConstant.ts
+++ src/contracts/royaltie/royaltieConstant.ts
@@ -1,5 +1,5 @@
 import fs from 'fs'
 export const royaltieClearB64 = "I3ByYWdtYSB2ZXJzaW9uIDYKaW50IDEKcmV0dXJu"
 export const royaltieB64 = Buffer.from(
-    fs.readFileSync("./RoyaltiePolicyApplicationTemplate_APPROVAL")
+    fs.readFileSync("./RoyaltiePolicyApplicationTemplate_APPROVAL.teal")
 ).toString("base64")
