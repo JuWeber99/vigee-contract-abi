d4c8c11eec9936b67d30b08f77670bfe0a71b61b
julian.weber <julian.weber@secuinfra.com>
2022-04-22 20:26:48 +0200

    testing

--- src/contracts/royaltie/RoyaltieApp.ts
+++ src/contracts/royaltie/RoyaltieApp.ts
@@ -92,7 +92,6 @@ export class RoyaltieApp extends BaseContract implements RoyaltieContract {
       numLocalInts: this.localSchema.numUint as number,
       numGlobalByteSlices: this.globalSchema.numByteSlice as number,
       numGlobalInts: this.globalSchema.numUint as number,
-
       signer: transactionSigner,
     });
     return atomicTransactionComposer
